4d37e6423e71d38e762afec5edfe7429eadaf58e
unknown <cahyakumaladewi16@gmail.com>
2024-11-04 14:50:57 +0700

    dropdwon profile

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,15 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+
+// Rute untuk logout
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// Rute untuk halaman home (jika dibutuhkan)
+Route::get('/home', function () {
+    return view('frontend.layout'); // Mengarahkan ke layout.blade.php
+})->name('home');
+
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
