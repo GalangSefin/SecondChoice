049682e332bfd7faafde9acb52f2b51b19c5f2f3
AlfitoDwi <titodwimirza@gmail.com>
2024-11-19 12:15:19 +0700

    update tampilan home

--- resources/views/frontend/layout.blade.php
+++ resources/views/frontend/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="author" content="Untree.co" />
+    <script src="https://cdn.tailwindcss.com"></script>
 
     <link rel="shortcut icon" href="favicon.png" />
 
