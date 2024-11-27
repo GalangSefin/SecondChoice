29c25439cbf8f131b9c899b2ff9a76a71aab7f73
Faradina <e31222662@student.polije.ac.id>
2024-11-27 13:56:52 +0700

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -13,7 +13,6 @@ class LoginController extends Controller
     use AuthenticatesUsers;
 
     protected $redirectTo = '/';
-    protected $redirectTo = '/';
 
     public function __construct()
     {
